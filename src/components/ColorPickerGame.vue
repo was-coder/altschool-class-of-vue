c64a38bbc0c8351ffd713892d3f483d39cc3337d
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2023-02-08 11:39:49 +0100

    feat:leverages the vue composite API by using composable for the colorPickerGame

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,36 +1,62 @@
 <template>
-  <h1>Color Picker Game</h1>
+  <div class="container">
+    <h1 class="header">Color Picker Game</h1>
 
-  <div>{{ message }}</div>
+    <div class="message">{{ message }}</div>
 
-  <button v-for="color in colors" :key="color" @click="matchColor(color)">
-    {{ color }}
-  </button>
+    <button
+      v-for="color in colors"
+      class="btn"
+      :key="color"
+      @click="matchColor(color)"
+    >
+      {{ color }}
+    </button>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "@vue/reactivity";
+import { colorManager } from '../composables/color-game-manager';
 
 export default {
   setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
+    const { colors, message, matchColor } = colorManager();
 
     return { colors, message, matchColor };
-  },
+  }
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+}
+
+.header {
+  font-weight: 600;
+  text-transform: uppercase;
+}
+
+.message {
+  padding: 20px 0;
+  font-size: 1.5rem;
+}
+
+.btn {
+  margin: 5px 0;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-transform: capitalize;
+  font-size: 1.2rem;
+}
+
+.btn:hover {
+  background-color: brown;
+  color: #fff;
+}
+</style>
